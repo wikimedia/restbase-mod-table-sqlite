0c40548ea5202bd9720617edb176a85fb3752570
Petr Pchelko <ppchelko@wikimedia.org>
2017-09-01 13:24:17 -0700

    Adapt the storage to the new storage requirements

--- test/index.js
+++ test/index.js
@@ -9,7 +9,7 @@ var fs = require("fs");
 describe('Functional', function() {
     var conf = yaml.safeLoad(fs.readFileSync(__dirname + '/test_client.conf.yaml'));
     var dbConstructor = require('../index.js');
-    require('restbase-mod-table-spec').test(function() {
+    require('restbase-mod-table-spec-ng').test(function() {
         return dbConstructor({
             conf: conf,
             log: function() {}
