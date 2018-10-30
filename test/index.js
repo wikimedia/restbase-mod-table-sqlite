0ddc1a8a311492dffe422ab4843f4c65494d3874
clarakosi <candrewwani@gmail.com>
2018-10-30 18:53:59 -0400

    updated dependencies
    Removed jshint and jscs and their files
    Updated dependencies for compatibility with node 4 and 6

--- test/index.js
+++ test/index.js
@@ -17,9 +17,5 @@ describe('Functional', function() {
     });
 });
 
-// Run jshint as part of normal testing
-require('mocha-jshint')();
-// Run jscs as part of normal testing
-require('mocha-jscs')();
 // Run eslint as part of normal testing
 require('mocha-eslint')([ 'lib', 'index.js' ]);
