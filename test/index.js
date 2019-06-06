642effcafb7c4d49ef2d5a88caf0e4d35074fa37
Holger Knust <hknust@wikimedia.org>
2019-06-06 14:29:10 -0400

    Replaced Istanbul with nyc
    Added coveralls and added mocha enabled eslint config file under tests
    
    Bug: T216191

--- test/index.js
+++ test/index.js
@@ -1,18 +1,15 @@
-"use strict";
-
-// mocha defines to avoid JSHint breakage
-/* global describe, it, context, before, beforeEach, after, afterEach */
+'use strict';
 
 var yaml = require('js-yaml');
-var fs = require("fs");
+var fs = require('fs');
 
-describe('Functional', function() {
+describe('Functional', function () {
     var conf = yaml.safeLoad(fs.readFileSync(__dirname + '/test_client.conf.yaml'));
     var dbConstructor = require('../index.js');
-    require('restbase-mod-table-spec').test(function() {
+    require('restbase-mod-table-spec').test(function () {
         return dbConstructor({
             conf: conf,
-            log: function() {}
+            log: function () {}
         });
     });
 });
